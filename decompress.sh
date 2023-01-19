dc0abf068f6b42515491e7499250be1b85bf76fa
Alex Massen-Hane <alex_mh23@outlook.com>
2023-01-19 13:45:18 +0800

    Update

--- decompress.sh
+++ decompress.sh
@@ -1,7 +1,20 @@
+# Copyright 2023 Curtin University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Alex Massen-Hane
+
 #!/bin/bash
-
-
-
 
 ### This script is to decompress all of the data from the Openaire data dump
 # Uses a combination of packages tar and gzip. You will need to delete the data/decompress folder and it's contents
