f1e2aa0e0c1619d2026f16009d8205e71221c745
Alex Massen-Hane <alex_mh23@outlook.com>
2023-12-14 06:44:41 +0000

    Update

--- openaire/model.py
+++ openaire/model.py
@@ -31,7 +31,7 @@ class Table:
     :param download_folder: Absolute path to where the part table tars files will be downloaded.
     :param decompress_folder: Absolute path to where the part table tars files will be decompressed.
     :param gcs_uri_pattern: Uri glob pattern of all the part files for the table in GCS.
-    :param alt_name: Altername name of the table part file on Zenodo. e.g. otherresearchproduct_1.tar but only 1 part.
+    :param alt_name: Alternative name of the table part file on Zenodo. e.g. otherresearchproduct_1.tar but only 1 part,
         so the file to download is otherresearchproduct_1.tar
     :param remove_nulls: Columns of where suspect nulls are that cause issues with importing to Bigquery.
     :param local_part_list_gz: List of where all the part files are locally stored (for the upload step).
